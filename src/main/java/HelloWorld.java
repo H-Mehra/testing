39a03711d37529cfbcf1db115b7524665b8d7abf
chainika <chainika@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2012-07-29 19:15:47 +0000

    this is revision after adding test file

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,40 @@
+public class HelloWorld{
+	
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello World to meera");
+		System.out.println("Hello World to Gurpinder");
+		System.out.println("Hello World to chainika");
+		System.out.println("Hello World to Rohan P");
+		helloFromKanan();
+
+		new HelloWorld().sayHelloToChainika();
+	
+
+	}
+	 public  void sayHelloToChainika(){
+	  		System.out.println("Hello Chainika!!!!");
+	 	}
+	 public static String helloFromKanan(){
+		 return "Hello From Kanan";
+	 }
+
+	public String helloMeera(){
+		String name = "Meera";
+		System.out.println("Hello From "+name);
+		return name;
+	}
+
+	public static void helloKomgrich() {
+		System.out.println("Hello Komgrich");
+	}
+	public void sayHello(){
+		System.out.println("Its intersting");
+			
+	}
+
+public static void helloPriya() {
+	System.out.println("Hello Priya");
+}
+}
