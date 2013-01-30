adf3a7bbd3b96c6e18d75f22ab153a12d9f8535c
rishi <rishi@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2013-01-30 19:04:16 +0000

    added parameterized runner

--- jus/src/test/java/com/q1a/ParameterizedTest.java
+++ jus/src/test/java/com/q1a/ParameterizedTest.java
@@ -6,9 +6,12 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 
+@RunWith(value=Parameterized.class)
 public class ParameterizedTest {
 
 	private double expected;
