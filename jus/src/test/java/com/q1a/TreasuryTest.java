cd3564772ed9cd2a60e21d4f752f6b70c5194f97
houston <houston@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2013-01-30 18:54:28 +0000

    base for TreasuryTest

---
+++ jus/src/test/java/com/q1a/TreasuryTest.java
@@ -0,0 +1,24 @@
+package com.q1a;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TreasuryTest {
+
+	@Test
+	public void testAddGold() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveGold() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGold() {
+		fail("Not yet implemented");
+	}
+
+}
