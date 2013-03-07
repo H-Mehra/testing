2faaa5535c0b8feabc4479b59fcaa62e596520e0
shruthi <shruthi@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2013-03-07 19:13:33 +0000

    Implemented PageFactory

--- pageobject/src/test/java/com/q1a/pageobject/LoginPage.java
+++ pageobject/src/test/java/com/q1a/pageobject/LoginPage.java
@@ -4,12 +4,31 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 
 public class LoginPage extends Page {
 	
+	
+	@FindBy(how=How.ID,using="img_login")
+	WebElement login;
+	
+	@FindBy(how=How.ID,using="UserName")
+	WebElement userName;
+	
+	@FindBy(how=How.ID,using="Password")
+	WebElement password;
+	
+	@FindBy(how=How.ID,using="Login")
+	WebElement loginButton;
+	
+	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	
 	
@@ -17,10 +36,10 @@ public class LoginPage extends Page {
 	public UserHomePage testLogin(){		
 		driver.get("http://173.200.182.212:9090/test.q1a/");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//*[@id='img_login']")).click();
-		driver.findElement(By.xpath("//*[@id='UserName']")).sendKeys("shruthi@infoobjects.com");
-		driver.findElement(By.xpath("//*[@id='Password']")).sendKeys("pass@1234");
-		driver.findElement(By.xpath("//*[@id='Login']")).click();
+		login.click();
+		userName.sendKeys("shruthi@infoobjects.com");
+		password.sendKeys("pass@1234");
+		loginButton.click();
 		
 		return new UserHomePage(driver);
 	}
