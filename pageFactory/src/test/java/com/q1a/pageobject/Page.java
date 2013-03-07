6a73937513a8fc083dc6743e7662daf2aed2ffe4
rishi <rishi@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2013-03-07 19:40:50 +0000

    made it generic

--- pageFactory/src/test/java/com/q1a/pageobject/Page.java
+++ pageFactory/src/test/java/com/q1a/pageobject/Page.java
@@ -1,8 +1,9 @@
 package com.q1a.pageobject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.LoadableComponent;
 
-public abstract class Page {
+public abstract class Page<T extends LoadableComponent<T>> extends LoadableComponent<T> {
 	
 	protected static WebDriver driver;
 
