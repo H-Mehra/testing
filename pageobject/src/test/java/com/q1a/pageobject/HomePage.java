cf4ad7ec07fddb0a827122a186c51acb5855324b
brian <brian@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2013-03-08 19:22:32 +0000

    reformat. Brian H.

--- pageobject/src/test/java/com/q1a/pageobject/HomePage.java
+++ pageobject/src/test/java/com/q1a/pageobject/HomePage.java
@@ -3,38 +3,30 @@ package com.q1a.pageobject;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends Page {
-	
-	
-	public HomePage(WebDriver driver){
+
+	public HomePage(WebDriver driver) {
 		this.driver = driver;
 
 	}
 
-	
 	public SignUpPage clickSignUpPage() {
 
 		return new SignUpPage(driver);
 	}
 
-	
 	public LoginPage clickLoginButton() {
 
 		return new LoginPage(driver);
 	}
 
-
 	public String getTitle() {
-		
-return driver.getTitle();
+
+		return driver.getTitle();
 	}
-	
 
 	public HiringManagerSignupPage clickHiringManagerSignup() {
-	
-	return new  HiringManagerSignupPage(driver);
+
+		return new HiringManagerSignupPage(driver);
 	}
-
-
-
 
 }
