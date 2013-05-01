b53e9b0a5f1cf42c36ec1eb04776ce70d6124080
spandana <spandana@df2dbb2b-60a1-7846-bc16-a4cc43dbcb54>
2013-05-01 17:56:32 +0000

    Added LoginDiv test case

--- webdriver/src/test/java/com/infoobjects/HomePageTest.java
+++ webdriver/src/test/java/com/infoobjects/HomePageTest.java
@@ -1,23 +1,65 @@
 package com.infoobjects;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HomePageTest {
 
-	@Test
-	public void test() {
-		// fail("Not yet implemented");
-		WebDriver driver = new FirefoxDriver();
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void init() {
+
+		driver = new FirefoxDriver();
 		driver.get("http://q1a.com");
+
+	}
+
+	@Test
+	public void testTitle() {
 		assertEquals("Title is not as expected",
 				"Technical Assessment Platform", driver.getTitle());
-		assertEquals("http://www.q1a.com/q1a", driver.getCurrentUrl());
-		driver.close();
+	}
 
+	@Test
+	public void testUrl() {
+		assertEquals("http://www.q1a.com/q1a", driver.getCurrentUrl());
+
+	}
+
+	@Test
+	public void testlogo() {
+
+		WebElement logo = driver.findElement(By.id("img_logo"));
+		assertNotNull(logo);
+		assertEquals("0", logo.getAttribute("border"));
+		assertEquals("Technical Assessment Platform",
+				logo.getAttribute("title"));
+
+	}
+
+	@Test
+	public void testLoginDiv() {
+
+		WebElement loginDiv = driver.findElement(By.id("1_fblogin"));
+
+		List<WebElement> divs = loginDiv.findElements(By.tagName("div"));
+
+	}
+
+	@AfterClass
+	public static void cleanUp() {
+		driver.close();
 	}
 
 }
